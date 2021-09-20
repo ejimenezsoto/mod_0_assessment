83f5ff3c936f81417374a9336fd9c708efa520c2
Enzo Jimenez-Soto <enzojimenezsoto@gmail.com>
2021-09-20 15:49:22 -0700

    Add modelB.js

---
+++ models/modelB.js
@@ -0,0 +1,10 @@
+var newDeveloper = {
+  firstName:"Enzo",
+  lastName:"Jimenez-Soto",
+  age:30,
+  favoriteNumber:27,
+  isOpenToWork: true,
+  currentlyWorking: false,
+  favoriteFoods:["Pizza","Ice-Cream","Steak and Lobster"],
+  programmingLanguages:["JavaScript","Swift","Python","Objective-C"]
+};
