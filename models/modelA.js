c9f93f5db2129fed0228d58423d5f904c6fcd0fa
Enzo Jimenez-Soto <enzojimenezsoto@gmail.com>
2021-09-20 15:44:00 -0700

    Add modelA.js

---
+++ models/modelA.js
@@ -0,0 +1,12 @@
+var tyrannosaurus = {
+  name:"Tyrannosaurus",
+  measurementsIn:"Feet",
+  averageHeight: 16,
+  averageLength: 40,
+  isStillAlive: false,
+  isCarnivore: true,
+  interestingFacts:["The Tyrannosaurus had a life span of around 30 years.",
+                    "Its arms were too short to reach its mouth.",
+                    "It lived in North America in river valleys and forests."],
+  foodSources:["Triceratops","Edmontosaurus"]
+};
