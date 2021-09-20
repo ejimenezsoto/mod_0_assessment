cf804288910e5c2a71b1fe278d022f2baa62f28a
Enzo Jimenez-Soto <enzojimenezsoto@gmail.com>
2021-09-20 15:29:44 -0700

    Complete program specific challenge

---
+++ variables.js
@@ -0,0 +1,17 @@
+var movie = "Empire Strikes Back";
+
+var isEntertaining = true;
+
+movie = "Logan";
+
+var num4 = 4;
+
+var num6 = 20;
+
+var product = num4 * num6;
+
+var favoriteFoods = ["Pizza","Ben and Jerrys Half-Baked","Ramen Nagi"];
+
+favoriteFoods.shift();
+
+console.log(movie.length);
